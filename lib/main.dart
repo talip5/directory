02fce0b439162d42b097790396f77fa7ab94b924
talip5 <talipefe35@gmail.com>
2021-10-26 11:20:57 +0300

    26.10.2021

--- lib/main.dart
+++ lib/main.dart
@@ -6,115 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'package:directory/models/functions.dart';
 
 void main() {
   runApp(myApp());
 }
 
 class myApp extends StatefulWidget {
-  const myApp({Key? key}) : super(key: key);
-
-  Future fileRead() async {
-    final folderName = 'dir';
-    final directoryPath =Directory("storage/emulated/0/Android/data/com.example.directory/files");
-    final filex=directoryPath.path;
-    final filey=File(filex+'counter.txt');
-    print(filex);
-    print(filey);
-   // return directory.path;
-  }
-
-  Future<String> get _localPath async {
-    //final directory = await getApplicationDocumentsDirectory();
-    //final directory = await getExternalStorageDirectory(); telefonda ve emulatorda kullanilan directory
-    final directory = await getExternalStorageDirectory();
-    // var directory=await Directory('dir').create(recursive: true);
-    print(directory);
-    return directory!.path;
-  }
-
-  Future _localPathCreate() async {
-    final folderName = 'dir';
-    //final directory =Directory("storage/emulated/0/Android/data/com.example.directory/files/$folderName");
-    final directory = Directory(
-        'storage/1000-3B08/Android/data/com.example.directory/files/$folderName');
-    if (await directory.exists()) {
-      print('exists');
-    } else {
-      print('not exists');
-      directory.create();
-    }
-    print(directory);
-  }
-
-  Future _localPathRead() async {
-    //final folderName = 'dir';
-    //final directory = Directory("storage/emulated/0/Android/data/com.example.directory/files/$folderName");
-    final directory=Directory('storage/1000-3B08/Android/data/com.example.directory/files');
-    //final directory=Directory('data/user/0/com.example.directory/app_flutter');
-    if (await directory.exists()) {
-      print('exists');
-      print(directory);
-      //path.delete();
-    } else {
-      print('not exists');
-      // path.create();
-    }
-    //print(path);
-  }
-
-  Future _localPathDeneme() async {
-    final directory = Directory(
-        "storage/emulated/0/Android/data/com.example.directory/files");
-    List contents = directory.listSync();
-    // Stream<FileSystemEntity> list({bool recursive = false, bool followLinks = true});
-    //final folderName='dir1';
-    //final directory=Directory("storage/emulated/0/Android/data/com.example.directory/files");
-    //final directory=Directory("storage/emulated/0/Android/data/com.example.directory/files/$folderName");
-    //final directory=Directory.current;   //  '/'
-    //final directory=Directory.systemTemp;  //Directory: '/data/user/0/com.example.directory/code_cache'
-    //final directory1=StorageDirectory.documents.;
-    //final directory = await getExternalStorageDirectory();
-    /*if(await directory.exists()){
-      print('exists');
-      print(directory);
-      print(directory.path);
-      print(directory.absolute);
-      print(directory.uri);
-      print(directory.parent);
-    }else{
-      print('not exists');
-      directory.create();
-    }*/
-    //print(directory);
-    //print(contents.length);
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/counter.txt');
-  }
-
-  Future<File> writeCounter(int counter) async {
-    final file = await _localFile;
-
-    // Write the file
-    return file.writeAsString('$counter');
-  }
-
-  Future<int> readCounter() async {
-    try {
-      final file = await _localFile;
-
-      // Read the file
-      final contents = await file.readAsString();
-
-      return int.parse(contents);
-    } catch (e) {
-      // If encountering an error, return 0
-      return 0;
-    }
-  }
+  Functions functions = Functions();
+  Functions functions1 = Functions();
+  List contents1 = [];
 
   @override
   _myAppState createState() => _myAppState();
@@ -122,33 +23,44 @@ class myApp extends StatefulWidget {
 
 class _myAppState extends State<myApp> {
 
-  List contents = [];
+  String textClass = 'textClass';
 
-  Future _localPathDirList() async {
-    final directory = Directory(
-        "storage/emulated/0/Android/data/com.example.directory/files");
-    contents = directory.listSync();
-    for (var fileOrDir in contents) {
-      if (fileOrDir is File) {
-        print(fileOrDir.toString());
-        setState(() {
-          fileOrDirx = fileOrDir.toString();
-        });
-      }
-      else if (fileOrDir is Directory) {
-        print(fileOrDir.toString());
-        setState(() {
-          fileOrDirx = fileOrDir.path;
-        });
-      }
-    }
-    //print(directory);
-    //print(contents.length);
+  //String initialValue = 'Kevin A.';
+  /* TextEditingController controller = new TextEditingController(
+      text: initialValue
+  );*/
+
+  /*controller.selection = new TextSelection(
+  baseOffset: 0,
+  extentOffset: initialValue.length,
+  );*/
+
+  TextEditingController myController = TextEditingController(text: initialValu);
+  String directoryName = 'directoryName';
+  Color textFieldColor = Colors.blueGrey;
+
+  static var initialValu = 'deneme';
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
   }
 
+  @override
   String asset1 = 'Assets';
   int counter = 1;
   String fileOrDirx = 'fileOrDir';
+
+  void yaz() {
+    print('textEditCompletioned');
+  }
+
+  colorTextEdit() {
+    setState(() {
+      textFieldColor = Colors.lightBlue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -159,49 +71,44 @@ class _myAppState extends State<myApp> {
         body: Center(
           child: Column(
             children: [
-              Text('xxxx'),
-              ElevatedButton(
-                  onPressed: () async {
-                    await widget.writeCounter(3);
-                    print(await widget.readCounter());
-                  },
-                  child: Text('String List')
+              Expanded(
+                child: ListView.builder(
+                  itemCount: widget.contents1.length,
+                    itemBuilder: (BuildContext context,int index){
+                    if(widget.contents1[index] is Directory){
+                      return ListTile(
+                        leading: Icon(Icons.folder),
+                        title: Text(widget.contents1[index].toString()),
+                      );
+                    }
+                    if(widget.contents1[index] is File){
+                      return ListTile(
+                        leading: Icon(Icons.file_present),
+                        title: Text(widget.contents1[index].toString()),
+                      );
+                    }
+                    return Text('bos');
+                // return Text(widget.contents1[index].toString());
+                    }
+                    ),
               ),
+              Text(textClass),
               ElevatedButton(
                   onPressed: () async {
-                    print(await widget._localPath);
-                    //await widget.writeCounter(3);
-                    //print(await widget.readCounter());
-                    print('Dir');
-                  },
-                  child: Text('Path Dir')
-              ),
-              Text(asset1),
-              ElevatedButton(
-                  onPressed: () async {
-                    await widget._localPathRead();
-                    print('Dir Read');
-                  },
-                  child: Text('Dir Read')
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    await widget._localPathCreate();
-                    print('Dir Create');
-                  },
-                  child: Text('Dir Create')
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    await widget._localPathDeneme();
+                    //textClass = widget.functions.localPathDeneme3();
+                    widget.contents1= await widget.functions.localPathDirList();
+                    //textClass=widget.contents1.length.toString();
+                    setState(() {
+                      textClass;
+                    });
                     print('Dir Deneme');
                   },
-                  child: Text('Dir Deneme')
-              ),
-              ElevatedButton(
+                  child: Text('Dir Deneme')),
+              /*ElevatedButton(
                   onPressed: () async {
                     //await widget._localPathDirList();
-                    await _localPathDirList();
+                   await _localPathDirList();
+                    //await widget.functions.localPathDirList();
                     print('Dir Liste');
                   },
                   child: Text('Dir Liste')
@@ -212,24 +119,106 @@ class _myAppState extends State<myApp> {
                     itemCount:contents.length,
                     itemBuilder:(BuildContext context, int index){
                       if(contents[index] is File){
-                        return ListTile(title:Text(contents[index].toString()));
+                        File? fileContents=contents[index];
+                        String fileName1=fileContents!.path;
+                        fileSingle=fileName1.split('/').last;
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 20.0,right: 20.0),
+                          child: Card(
+                            color: Colors.grey,
+                            child: ListTile(
+                              leading: Icon(Icons.file_present),
+                              onTap: (){
+                                //print(contents[index].toString());
+                                print(fileSingle);
+                              },
+                               // title:Text(contents[index].toString())),
+                                title:Text(fileSingle)),
+                          ),
+                        );
                       }
                       else if(contents[index] is Directory){
+                        Directory? dirContents=contents[index];
+                      String dirName1=dirContents!.path;
+                        dirSingle=dirName1.split('/').last;
                         //return Text(contents[index].toString());
-                        return ListTile(title:Text(contents[index].toString()));
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 20.0,right: 20.0),
+                          child: Card(
+                            color: Colors.blueGrey,
+                            child: ListTile (
+                                leading: Icon(Icons.store_mall_directory_outlined),
+                                onTap: () async{
+                                    print(dirSingle);
+                                    deleteDirectory=contents[index];
+                                    deleteDirectory!.delete().then((value) =>{
+                                      setState(() {
+                                        _localPathDirList();
+                                      })
+                                    });
+                                },
+                                //title:Text(contents[index].toString())),
+                              title:Text(dirSingle),
+                          ),
+                        ),
+                        );
                       }
                       return Text('bos');
                     }
                 ),
               ),
-              ElevatedButton(
-                  onPressed: () async {
-                    //await widget._localPathDirList();
-                    await widget.fileRead();
-                    print('File Read');
-                  },
-                  child: Text('File Read')
+              Row(
+                children: [
+                 SizedBox(width: 20.0),
+                  //Text(),
+                  Expanded(
+                    child: TextField(
+                      onChanged:(value){
+                        print(value);
+                        setState(() {
+                          textFieldColor=Colors.lightGreen;
+                        });
+                      },
+                      onEditingComplete: yaz,
+                      controller: myController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: textFieldColor,
+                        border: OutlineInputBorder(),
+                        labelText: directoryName,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20.0),
+                  ElevatedButton(
+                      onPressed: () async {
+                        if(myController.text.isNotEmpty) {
+                          String xfolderName = await widget.folderName;
+                          xfolderName = myController.text;
+                          //await widget._localPathDirList(xfolderName);
+                          await widget._localPathCreate(xfolderName).then((
+                              value) =>
+                          {
+                            setState(() {
+                              _localPathDirList();
+                              myController.text = '';
+                              textFieldColor=Colors.lightBlue;
+                            })
+                          });
+                          print('Create Directory ');
+                          print(myController.text);
+                        }else {
+                          setState(() {
+                            directoryName='directoryName enter';
+                            textFieldColor=Colors.redAccent;
+                          });
+                        }
+                      },
+                      child: Text('Create Directory')
+                  ),
+                ],
               ),
+             // SizedBox(height:50.0),*/
             ],
           ),
         ),
---
+++ lib/models/functions.dart
@@ -0,0 +1,218 @@
+import 'dart:async' show Future;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+
+class Functions {
+
+  String name = 'Ali';
+  final folderName = 'dir';
+
+  List contents = [];
+  Directory? deleteDirectory;
+  String dirSingle = '';
+  String fileSingle = '';
+
+
+  oku() {
+    print('Oku');
+  }
+
+  String localPathDeneme3(){
+    final folderName = 'dir';
+    final directory = Directory(
+        "storage/emulated/0/Android/data/com.example.directory/files/$folderName");
+    dirSingle=directory.path;
+    /*if (await directory.exists()) {
+      print('exists');
+      directory.delete();
+    } else {
+      print('not exists');
+      directory.create();
+    }*/
+    print(directory);
+    return dirSingle;
+  }
+
+  Future<List> localPathDirList() async {
+    final directory = Directory("storage/emulated/0/Android/data/com.example.directory/files");
+    contents = directory.listSync();
+    for (var fileOrDir in contents) {
+      if (fileOrDir is File) {
+        print(fileOrDir.toString());
+      }
+      else if (fileOrDir is Directory) {
+        print(fileOrDir.toString());
+      }
+    }
+    return contents;
+  }
+
+
+/*Future localPathDirList() async {
+    final directory = Directory("storage/emulated/0/Android/data/com.example.directory/files");
+    contents = directory.listSync();
+    for (var fileOrDir in contents) {
+      if (fileOrDir is File) {
+        print(fileOrDir.toString());
+        setState(() {
+          fileOrDirx = fileOrDir.toString();
+        });
+      }
+      else if (fileOrDir is Directory) {
+        print(fileOrDir.toString());
+        setState(() {
+          fileOrDirx = fileOrDir.path;
+        });
+      }
+    }
+    //print(directory);
+    //print(contents.length);
+  }*/
+
+ /* Future fileRead() async {
+    final folderName = 'dir';
+    final directoryPath =Directory("storage/emulated/0/Android/data/com.example.directory/files");
+    final filex=directoryPath.path;
+    final filey=File(filex+'counter.txt');
+    print(filex);
+    print(filey);
+    // return directory.path;
+  }*/
+
+  /*Future localPathCreate(String folderName) async {
+
+    final directory =Directory("storage/emulated/0/Android/data/com.example.directory/files/$folderName");
+    //final directory = Directory('storage/1000-3B08/Android/data/com.example.directory/files/$folderName');
+    if (await directory.exists()) {
+      print('exists');
+    } else {
+      print('not exists');
+      directory.create();
+    }
+    print(directory);
+  }*/
+
+  /*Future<String> get _localPath async {
+    //final directory = await getApplicationDocumentsDirectory();
+    //final directory = await getExternalStorageDirectory(); telefonda ve emulatorda kullanilan directory
+    final directory = await getExternalStorageDirectory();
+    // var directory=await Directory('dir').create(recursive: true);
+    print(directory);
+    return directory!.path;
+  }*/
+
+
+
+  /*Future _localPathCreate1() async {
+    final folderName = 'dir';
+    final directory =Directory("storage/emulated/0/Android/data/com.example.directory/files/$folderName");
+    //final directory = Directory('storage/1000-3B08/Android/data/com.example.directory/files/$folderName');
+    if (await directory.exists()) {
+      print('exists');
+    } else {
+      print('not exists');
+      directory.create();
+    }
+    print(directory);
+  }*/
+
+  /*Future _localPathRead() async {
+    //final folderName = 'dir';
+    //final directory = Directory("storage/emulated/0/Android/data/com.example.directory/files/$folderName");
+    final directory=Directory('storage/1000-3B08/Android/data/com.example.directory/files');
+    //final directory=Directory('data/user/0/com.example.directory/app_flutter');
+    if (await directory.exists()) {
+      print('exists');
+      print(directory);
+      //path.delete();
+    } else {
+      print('not exists');
+      // path.create();
+    }
+    //print(path);
+  }*/
+
+  /*Future _localPathDeneme2() async {
+    final directory = Directory("storage/emulated/0/Android/data/com.example.directory/files");
+    //List contents = directory.listSync();
+    List contents =await directory.listSync();
+
+    print(contents.length);
+    print(contents.first.toString());
+    print(contents.isEmpty);
+    print(contents.iterator.runtimeType);
+    print(contents.toList().length);
+    // print(contents.);
+    //Directory dir2=Directory('storage/emulated/0/Android/data/com.example.directory/files/dir2');
+    //contents.add(dir2);
+  }*/
+
+  /*Future _localPathDeneme() async {
+    // final evenNumbers = myList1.where((element) => element.isEven);
+    //print(evenNumbers);
+    String dirName5;
+    final folderName = 'dir';
+    final directory = Directory(
+        "storage/emulated/0/Android/data/com.example.directory/files/$folderName");
+    dirName5 = directory.path;
+    var dirSingle5 = dirName5.split('/').elementAt(3);
+    print(dirName5);
+    print(dirSingle5);
+    print(dirName5
+        .split('/')
+        .length);
+  }*/
+
+  /*Future _localPathDeneme1() async {
+    final directory = Directory("storage/emulated/0/Android/data/com.example.directory/files");
+    final myList = [2, 4, 6];
+    for (var number in myList) {
+      print(number);
+    }
+    *//*final myMap = {'a': 1, 'b':2, 'c':3};
+    for (var element in myMap) {
+      print(element);
+    }*//*
+
+    final myMap = {'a': 1, 'b':2, 'c':3};
+    for (var key in myMap.keys) {
+      print('key: $key, value: ${myMap[key]}');
+      // print('value --- ${myMap[key]}');
+    }
+
+    const myList1 = [1, 2, 3, 4, 5, 6, 7, 8];
+    final evenNumbers = myList1.where((element) => element.isEven);
+    print(evenNumbers);
+    print(evenNumbers.toList());
+
+    Iterable<int> iterable = [1, 2, 3];
+    int value = iterable.elementAt(1);
+    print(value);
+  }*/
+
+  /*Future _localPathDirList1() async {
+    final directory = Directory(
+        "storage/emulated/0/Android/data/com.example.directory/files");
+    contents = directory.listSync();
+    for (var fileOrDir in contents) {
+      if (fileOrDir is File) {
+        print(fileOrDir.toString());
+        setState(() {
+          fileOrDirx = fileOrDir.toString();
+        });
+      }
+      else if (fileOrDir is Directory) {
+        print(fileOrDir.toString());
+        setState(() {
+          fileOrDirx = fileOrDir.path;
+        });
+      }
+    }
+    //print(directory);
+    //print(contents.length);
+  }*/
+}
